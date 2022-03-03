dbac31df84686cebbb03985d077d83e6040db7d1
Practice <josephp123@sky.com>
2022-03-03 17:11:07 +0000

    Java Back-End and MySQL integrated

---
+++ src/main/resources/player-schema.sql
@@ -0,0 +1,5 @@
+create database project;
+use project;
+create table player (id integer AUTO_INCREMENT, name varchar(40), position varchar(40), team varchar(40), number integer(11), primary key (id));
+
+select * from player;
